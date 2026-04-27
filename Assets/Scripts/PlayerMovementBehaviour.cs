221cf609d444bd98a0980db0cbae1f17d937b575
dev-aghad <ghadialiarmaan@gmail.com>
2026-04-27 14:34:42 +0100

    Fixed movement bug
    Made it so you cannot move downwards and jumping now has a cooldown (to prevent character falling over and slipping)

--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -4,15 +4,20 @@ using UnityEngine.InputSystem;
 public class PlayerMovementBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed;
-    
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpCooldown = 5f;
+
     private InputAction moveAction;
     private Transform tr;
     private Vector2 axisValue;
     private Vector2 moveDistance;
+    private float lastJumpTime;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         tr = transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Not in Awake since we want to make sure external peripherals and controllers are initialised before starting 
@@ -26,8 +31,17 @@ public class PlayerMovementBehaviour : MonoBehaviour
         if (moveAction != null) 
         {
             axisValue = moveAction.ReadValue<Vector2>();
-            moveDistance = axisValue * speed * Time.deltaTime;
+
+            // Horizontal movement
+            moveDistance = new Vector2(axisValue.x, 0) * speed * Time.deltaTime;
             tr.Translate(moveDistance);
+
+            // Vertical movement
+            if (axisValue.y > 0.1f && Time.time >= lastJumpTime + jumpCooldown)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                lastJumpTime = Time.time;
+            }
         }
     }
     public Vector2 GetMovementInput()
